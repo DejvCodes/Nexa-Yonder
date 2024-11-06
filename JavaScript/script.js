c22e2be7a4bedce135780f47ba81e97fb01bd73a
David Kalmus <dejvios@seznam.cz>
2024-11-06 18:24:19 +0100

    JavaScript/

---
+++ JavaScript/script.js
@@ -0,0 +1,27 @@
+// ----------------------- SHOW MENU -----------------------
+const burgerMenu = document.getElementById('burger-menu');
+const nav = document.getElementById('nav-links');
+const navLinks = document.querySelectorAll('.nav-links a');
+
+burgerMenu.addEventListener('click', () => {
+    showMenu();
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        showMenu();
+    });
+});
+
+const showMenu = () => {
+    nav.classList.toggle('nav-active');
+    burgerMenu.classList.toggle('toggle');
+
+    navLinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.3s ease forwards ${index / 7 + 0.3}s`;
+        }
+    });
+};
